51e8e624fcabf31b24bc94e813387c66560f3233
gitartikONDER <30984565+gitartikONDER@users.noreply.github.com>
2017-12-30 16:36:12 +0300

    test

--- TabloKarsilastirma/src/application/Main.java
+++ TabloKarsilastirma/src/application/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+//main class
 
 public class Main extends Application {
 	@Override
